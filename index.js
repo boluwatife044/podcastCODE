e7b61a3480db23a551ccc202f9cb2c2a19ab3ac2
adeniyi excel <adeniyiexcel@gmail.com>
2023-08-05 17:09:44 +0100

    contact function

---
+++ index.js
@@ -0,0 +1,12 @@
+function Bolu() {
+    //pic = document.getElementById('imgv');
+    //pic.innerHTML = "My name is bolu"
+    pict = document.getElementById('changeImg');
+    pict.src = './undraw_Finance_re_gnv2.png';
+}
+
+function Contact(){
+    const contactInfo = document.getElementById('contact-effect');
+    contactInfo.className = 'style-container-show-the-form';
+
+}
